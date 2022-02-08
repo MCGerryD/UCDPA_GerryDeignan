601023a8443f15adc925ee330dfba10de0e7fdae
MCGerryD <gerrytdeignan@gmail.com>
2022-02-08 19:28:32 +0000

    Read in Dublin Bikes data and set datatypes for Time and Station_ID Extracted Date from the Time field Sorted by Station_ID and Time and took the difference of Available Bikes and Prev row to give Interactions/Taken

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+# High-Level of Steps I need:
+# 1. Determine how many Dublin Bikes were used per day
+# 2. Merge the Weather data for each day
+# 3. Compare Bike usage level to Weather conditions
+# 4. Use this comparison to create a model that will forecast demand based on Weather forecast for the week ahead
 
 import requests
 import numpy as np
@@ -32,3 +36,24 @@ print(initial_df.head())
 print(initial_df.info())
 print(initial_df.describe())
 
+# (1.)
+# To determine number of bikes used each day I need to extract the date from the Time field
+# Using Pandas DatetimeIndex to extract its and store in DATE
+initial_df['DATE'] = pd.DatetimeIndex(initial_df['TIME']).date
+# Check new field is as expected
+print(initial_df['DATE'])
+print(initial_df.head())
+
+#Sort the dataframe so that I can get the final sum of bike usage for each day (end of day will have the cumulative sum)
+initial_df = initial_df.sort_values(['STATION ID', 'TIME'], ascending=(True, True))
+print(initial_df.head())
+
+# Initial_df now has the number of bikes that are available at each 5 minute interval
+# sorted by Station_ID and Time.
+# To calculate the total number times bikes are taken each day I am going to
+# subtract the value in Available Bikes from the value in previous row (5 mins before).
+# Each time the difference is negative will be a proxy for the number of bikes taken in that
+# 5 minute window. I am going to assume that the sum of  these negative values for an entire day
+# will be the number of bikes taken in that day
+initial_df['Interactions']=initial_df.groupby(['STATION ID', 'DATE'])['AVAILABLE BIKES'].diff().fillna(0)
+print(initial_df.head())
